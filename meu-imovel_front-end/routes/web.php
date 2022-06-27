58d2977466b0339d01b3bcea5b886d73e49b29db
douglasbispo <dsanttos363@gmail.com>
2022-06-27 11:58:02 -0300

    Enviando projeto

---
+++ meu-imovel_front-end/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\{
+    ImovelController,
+    UsuarioController,
+};
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', [ImovelController::class, 'index']);
+Route::get('/imovel/{id}', [ImovelController::class, 'show']);
+Route::get('/cadastrar', [ImovelController::class, 'create']);
+Route::post('/imovel', [ImovelController::class, 'store']);
+Route::get('/dashboard', [ImovelController::class, 'dashboard']);
+Route::get('/editar/{id}', [ImovelController::class, 'edit']);
+Route::put('/atualizar/{id}', [ImovelController::class, 'update']);
+Route::delete('/apagar/{id}', [ImovelController::class, 'destroy']);
+
+
+# User
+Route::get('/login', [UsuarioController::class, 'login']);
+Route::post('/signin', [UsuarioController::class, 'signin']);
